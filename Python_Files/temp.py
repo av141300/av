1cf72866ba3b814e6014682e0c036458ac42119b
Andrey Dmitriev <avdmitriev.sp@yandex.ru>
2020-02-26 18:30:04 +0300

    Приличное обновление кода
    Дописаны много функций и вывод в файл

--- Python_Files/temp.py
+++ Python_Files/temp.py
@@ -1,10 +1,11 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
 site = 'https://www.eurookna.ru'
-url = 'https://www.eurookna.ru/okna/steklopaket-na-zakaz-bez-ramy/1213/'
+url = 'https://www.eurookna.ru/balcony/plastikovye-balkon/'
 author = 'eurookna.ru'
 code = "cp1251"
 noimage = '/local/templates/201907/images/banner_2020_small.jpg'
+phone = '+7(495)725-60-65'
 
 def get_html(url):
     r = requests.get(url)    # Получаем метод Response
@@ -40,6 +41,37 @@ def get_preview_text(html):
         preview_text = ''
     return preview_text
 
+def get_small_seo_text_h2(html):
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        small_seo_text_h2 = soup.find('section', class_='turbo-small-text').find('h2').text.strip()
+    except:
+        small_seo_text_h2 = ''
+    return small_seo_text_h2
+
+def get_small_seo_text(html):
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        small_seo_text = soup.find('section', class_='turbo-small-text').find('h2').findNext('div').text.strip()
+    except:
+        small_seo_text = ''
+    return small_seo_text
+
+def if_empty_p(text):
+     soup = BeautifulSoup(text, 'lxml')
+     return re.sub(r'\s+', ' ', text.strip())
+
+
+def get_seo_text(html):
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        seo_text = soup.find('div', class_='turbo-article')
+    except:
+        seo_text = ''
+    return seo_text
+
+# Собираем данные
+
 def parse(data):
     results = []
     soup = BeautifulSoup(data, 'lxml')
@@ -48,21 +80,30 @@ def parse(data):
     title = soup.title.string.strip()
     description = soup.findAll(attrs={"name":"description"})[0]['content'].strip()
     main_image_url = get_image(data)
-    preview_text = get_preview_text(data)
+    preview_text = if_empty_p(get_preview_text(data))
+    small_seo_text_h2 = get_small_seo_text_h2(data)
+    small_seo_text = if_empty_p(get_small_seo_text(data))
+    seo_text = if_empty_p(str(get_seo_text(data)))
     results.append({
         'title': title,
         'h1': h1,
         'description' : description,
         'main_image_url' : main_image_url,
-        'preview_text' : preview_text
+        'preview_text' : preview_text,
+        'small_seo_text_h2' : small_seo_text_h2,
+        'small_seo_text' : small_seo_text,
+        'seo_text' : seo_text
     })
     return results
 
 data = get_html(url)
 a = parse(data)
 
+#print(re.sub(r'\s+', ' ', str(get_seo_text(data))).strip())
+
 with open('temp.xml', 'w') as output_file:
     output_file.write(
+
 '''<?xml version="1.0" encoding="'''+ code + '''"?>
 <rss xmlns:yandex="http://news.yandex.ru" 
      xmlns:media="http://search.yahoo.com/mrss/" 
@@ -82,29 +123,30 @@ with open('temp.xml', 'w') as output_file:
                             <img src="''' + site + a[0]['main_image_url'][0] + '''">
                         </figure>
                         <p>'''+ a[0]['preview_text'] +'''</p>
-                        <menu>
-                           <a href="http://example.com/page1.html">53453</a>
-                           <a href="http://example.com/page2.html">2342</a>
-                        </menu>
+                        <button
+                          formaction="tel:'''+ phone +'''"
+                          data-background-color="red"
+                          data-color="white"
+                          data-turbo="false"
+                          data-primary="true"
+                        >
+                        '''+ phone +
+                        '''
+                        </button>
                     </header>
-                    <figure>
-                       <img src="https://avatars.mds.yandex.net/get-sbs-sd/369181/49e3683c-ef58-4067-91f9-786222aa0e65/orig">
-                        <figcaption></figcaption>
-                    </figure>
-                   
-                    <button formaction="tel:+7(123)456-78-90"
-                            data-background-color="#5B97B0"
-                            data-color="white"
-                            data-primary="true"></button>
-                    <div data-block="widget-feedback" data-stick="false">
-                        <div data-block="chat" data-type="whatsapp" data-url="https://whatsapp.com"></div>
-                        <div data-block="chat" data-type="telegram" data-url="http://telegram.com/"></div>
-                        <div data-block="chat" data-type="vkontakte" data-url="https://vk.com/"></div>
-                        <div data-block="chat" data-type="facebook" data-url="https://facebook.com"></div>
-                        <div data-block="chat" data-type="viber" data-url="https://viber.com"></div>
-                    </div>
-                    <p><a href="#">Nullam dolor massa, porta a nulla in, ultricies vehicula arcu.</a></p>
-                    <p>http://unsplash.com</p>
+                    <h2>'''+ a[0]['small_seo_text_h2'] +'''</h2>
+                    <p>''' + a[0]['small_seo_text'] + '''</p>
+                    '''+ a[0]['seo_text'] +'''
+                    <button
+                          formaction="tel:'''+ phone +'''"
+                          data-background-color="red"
+                          data-color="white"
+                          data-turbo="false"
+                          data-primary="true"
+                        >
+                        '''+ phone +
+                        '''
+                    </button>
                 ]]>
             </turbo:content>
         </item>
